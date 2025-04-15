e53244d29713ebe70d6d5c06c1ee1946aa000e0f
UNNATISONDHIYA <sondhiyaunnati02@gmail.com>
2025-04-15 18:36:52 +0530

    java

--- Pattern.java
+++ Pattern.java
@@ -1,13 +1,15 @@
 public class Pattern {
     public static void main(String[] args) {
-       // for (int i = 1; i <= 5; i++) {
-           // for (int j = 1; j<=i; j++) {
+        for (int i = 1; i <= 5; i++) {
+         for (int j = 1; j<=i; j++) {
                 
-           // System.out.print("*");
-          //  }
-           // System.out.println();
-       // }
-   // } 
+           System.out.print("*");
+          }
+         System.out.println();
+       }
+    }
+}
+   //} 
    // for (int i = 1; i <= 5; i++) {
        // for (int j = 5; j>=i; j--) {
             
@@ -30,24 +32,23 @@ public class Pattern {
    // }
    // System.out.println();  
 //}
-for (int i = 1; i <= 5; i++) {
+// for (int i = 1; i <= 5; i++) {
     
-    for (int j = 1; j<i; j++) {
+//     for (int j = 1; j<i; j++) {
         
-    System.out.print(" ");
-    }
-for (int k=5;k>i;k--)
-{
-    System.out.print("*");
-}
-for (int p=5;p>=i;p--)
-{
-    System.out.print("*");
-}
+//     System.out.print(" ");
+//     }
+// for (int k=5;k>i;k--)
+// {
+//     System.out.print("*");
+// }
+// for (int p=5;p>=i;p--)
+// {
+//     System.
 
-    System.out.println();
-}
-}
-}
+//     System.out.println();
+// }
+// }
+// }
 
     
